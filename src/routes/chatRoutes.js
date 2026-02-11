e691676afe1a9fd3e350443f92c47500c8beec77
devitachiui22 <kuandashopp@gmail.com>
2026-02-11 14:20:43 +0100

    Delete src directory

--- src/routes/chatRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../controllers/chatController');
-const { authenticateToken } = require('../middleware/auth');
-
-router.get('/:ride_id', authenticateToken, controller.getHistory);
-
-module.exports = router;
